243da9d29d63018a87876d8d61adb960bbf050de
ayushi1411 <bknashi@gmail.com>
2017-02-14 01:49:13 +0530

    joins implemented

--- main.py
+++ main.py
@@ -46,14 +46,15 @@ def selectQuery(cols, tables): # implements the select part of the query
 			if i in columnNumber:
 				dt.append(int(row[i]))
 		reqData.append(dt)
-#	displayOutput(reqData , cols, tableName)
 	return reqData
 
 def displayOutput(data, cols, tableName): #displays the required final output
 	lineWidth = 20
+	i = 0
 	for it in cols:
-		header = tableName + "." + it
+		header = tableName[i] + "." + it
 		print header.ljust(lineWidth),
+		i+=1
 	print
 	for row in data:
 		for ele in row:
@@ -102,52 +103,129 @@ def processQuery(query):
 		for ele in collist:
 			columns.append(ele)
 
-	#get data for required columns
-	selectData = selectQuery(columns,tables)
+	if len(tables) == 1:
+		#get data for required columns
+		selectData = selectQuery(columns,tables)
 
-	#delete repeated data in case of distinct
-	reqData = []
-	if flagDistinct == 1:
-		for ele in selectData:
-			if ele not in reqData:
-				reqData.append(ele)
-
-	elif flagAggregate == 1:
-		if(len(selectData[0]) > 1):
-			print "only one column allowed in aggregate functions"
-		
-		else : 			
-			l1 = []
+		#delete repeated data in case of distinct
+		reqData = []
+		if flagDistinct == 1:
 			for ele in selectData:
-				for val in ele:
-					l1.append(val)
+				if ele not in reqData:
+					reqData.append(ele)
+
+		elif flagAggregate == 1:
+			if(len(selectData[0]) > 1):
+				print "only one column allowed in aggregate functions"
+		
+			else : 			
+				l1 = []
+				for ele in selectData:
+					for val in ele:
+						l1.append(val)
 
 
-			if func == "max":
-				l = []
-				l.append(max(l1)) 
-				reqData.append(l)
+				if func == "max":
+					l = []
+					l.append(max(l1)) 
+					reqData.append(l)
 
-			elif func == "min":
-				print l1
-				l = []
-				l.append(min(l1)) 
-				reqData.append(l)
+				elif func == "min":
+					print l1
+					l = []
+					l.append(min(l1)) 
+					reqData.append(l)
 
-			elif func == "sum":
-				l = []
-				l.append(sum(l1)) 
-				reqData.append(l)
+				elif func == "sum":
+					l = []
+					l.append(sum(l1)) 
+					reqData.append(l)
 
-			elif func == "avg":
-				l = []
-				l.append(sum(l1)/(float)(len(l1))) 
-				reqData.append(l)
+				elif func == "avg":
+					l = []
+					l.append(sum(l1)/(float)(len(l1))) 
+					reqData.append(l)
+		else:
+			for ele in selectData:
+				reqData.append(ele)
+		tables = tables * len(columns)
+
+			
+
 	else:
-		for ele in selectData:
-			reqData.append(ele)
+		whereClause = str(stmt.tokens[-1])
+		whereClause = whereClause.split(' ')
+		#join
+		if len(tables) == 2:
+			if len(whereClause) == 2:
+				joinStmt = whereClause[1]
+				whereClause = whereClause[1].split('=')
+				clm1 = whereClause[0]
+				clm2 = whereClause[1]
+				tbl1 = clm1.split(".")[0]
+				clm1 = clm1.split(".")[1]
+				tbl2 = clm2.split(".")[0]
+				clm2 = clm2.split(".")[1]
+				clm2 = clm2.split(";")[0]
+				ct1 = []
+				ct2 = []
+				if columnToken == '*':
+					for ele in schema[tables[1]]:
+						if ele not in columns:
+							columns.append(ele)
+				for c in columns:
+					if c in schema[tables[0]]:
+						ct1.append(c)
+					if c in schema[tables[1]]:
+						ct2.append(c)
+				if clm2 in ct2:
+					ct2.remove(clm2)
+				table1 = []
+				table1.append(tables[0])	
+				table2 = []
+				table2.append(tables[1])	
+				selectData1 = selectQuery(ct1,table1)
+				selectData2 = selectQuery(ct2,table2)
+				columnsOfTable = schema[tables[0]] #the columns in the table
+				col = []
+				for ele in columnsOfTable:
+					col.append(ele)
+				dt1 = selectQuery(col, table1)
 
-	displayOutput(reqData, columns, tables[0])
+				columnsOfTable = schema[tables[1]] #the columns in the table
+				col = []
+				for ele in columnsOfTable:
+					col.append(ele)
+				dt2 = selectQuery(col, table2)
+
+				pos1 = schema[tables[0]].index(clm1)
+				pos2 = schema[tables[1]].index(clm2)
+
+				rowNumbers1 = []
+				rowNumbers2 = []
+				i = 0
+				for ele1 in dt1:
+					j=0
+					for ele2 in dt2:
+						if ele1[pos1] == ele2[pos2]:
+							rowNumbers1.append(i)
+							rowNumbers2.append(j)
+						j+=1
+					i+=1
+				reqData = []
+				for i in range(len(rowNumbers1)):
+					temp = []
+					for e in selectData1[rowNumbers1[i]]:
+						temp.append(e)
+					for e in selectData2[rowNumbers2[i]]:
+						temp.append(e)
+					reqData.append(temp)
+			tables = table1 * len(ct1)
+			temptb = []
+			temptb = table2[0] * len(ct2)
+			tables.append(temptb)
+#			print reqData
+	displayOutput(reqData, columns, tables)
 
 
 if __name__ == "__main__":
@@ -160,3 +238,17 @@ if __name__ == "__main__":
 	getSchema()
 	processQuery(query)
 
+'''		whereClause = str(stmt.tokens[-1])
+		whereClause1 = whereClause.split(' ')
+		op = ['>', '<', '=', '<=', '>=', '!=']
+		whereClause = re.split('[> < = <= >= !=]',whereClause1)
+		if '' in whereClause:
+			whereClause.remove('')
+		clm = whereClause[0]
+		num = int(whereClause[1])
+		columnstemp = []
+		for ele in schema[tables[0]]:
+			columnstemp.append(ele)
+		postemp = schema[tables[0]].index(clm)
+		sdata = selectQuery(columnstemp,tables)
+		'''
